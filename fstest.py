690ef56742c246e3c487e1ec0062617bb9eeee89
jwfoster64 <jmfoster7@gmail.com>
2022-01-23 21:48:48 -0700

    Update fstest.py

--- fstest.py
+++ fstest.py
@@ -9,70 +9,96 @@ from selenium.webdriver.support.select import Select
 browser = webdriver.Safari()
 browser.maximize_window()
 browser.implicitly_wait(5)
+
+#Go to fullstory.com/login, add device cookie
 browser.get('https://app.fullstory.com/login/')
 cookie = {'name': 'fs_trusted_device', 'value': 'DvS6MrpikMXlrYouVqcS2Q=='}
 browser.add_cookie(cookie)
 
 time.sleep(3)
 try:
-    element = browser.find_element_by_xpath('//*[@id="login-container"]/div/div/div[2]/div/div[2]/form/div[1]/div/div')
-    element.click()
-    element.send_keys('jmfoster7@gmail.com')
+    #send login email keys  
+    email = browser.find_element_by_xpath('//*[@id="login-container"]/div/div/div[2]/div/div[2]/form/div[1]/div/div')
+    email.click()
+    email.send_keys('jmfoster7@gmail.com')
+    
     browser.find_element_by_xpath('//*[@id="login-container"]/div/div/div[2]/div/div[2]/form/div[2]/button').click()
-    text = browser.find_element_by_xpath('//*[@id="login-container"]/div/div/div[2]/div/div[2]/form/div[1]'
+    
+    passwd = browser.find_element_by_xpath('//*[@id="login-container"]/div/div/div[2]/div/div[2]/form/div[1]'
                                             '/div/div[2]/div')
-    text.click()
-    text.send_keys('Testauto1')
+    #send password keys
+    passwd.click()
+    passwd.send_keys('Testauto1')
     browser.find_element_by_xpath('//*[@id="login-container"]/div/div/div[2]/div/div[2]/form/div[2]/button/span')\
         .click()
 
+    #Login successful screenshot
     browser.save_screenshot("login_pass.png")
     print('Login successful')
 
 except:
 
+    #Login failed screenshot
     browser.save_screenshot("login_fail.png")
     print('Login failed')
 
 time.sleep(5)
 try:
 
+    #Click play button of recorded session
     playButton = browser.find_element_by_xpath('//div[@class="_12nOgIHbvYG4xSFAdhxiEb UNs8LeRHYzGmwk9AoMo93"]')
     playButton.click()
 
+    #Play button selected succesfully
     browser.save_screenshot("play_recording.png")
     print('Play recording successful')
 
 except:
 
+    #Play button select failed
     browser.save_screenshot("play_recording_fail.png")
     print('Play recording fail')
 
 time.sleep(5)
 try:
 
+    #Select Note and Share button and add note
     share = browser.find_element_by_id('share')
     share.click()
     testShare = browser.find_element_by_xpath('//textarea[@placeholder="Leave a note about this session"]')
     testShare.send_keys('Test Note Automation')
+    
+    #Create Note
     note = browser.find_element_by_xpath('//button[@class="share"]')
     note.click()
+    
+    #Go back to home page
     bte = browser.find_element_by_xpath('//a[@class="backBtn"]')
     bte.click()
+    
+    #Select Notes tab
     notesButton = browser.find_element_by_xpath('//a[@id="highlightsBtn"]')
     notesButton.click()
+    
+    #Select JIRA link on created note
     jira = browser.find_element_by_xpath('//span[contains(text(),"Jira")]')
     jira.click()
+    
+    #Add additional details to summary field
     summary = browser.find_element_by_xpath('//input[@type="text"]')
     summary.send_keys('test note sent')
+    
+    #Click Send button to send Note to JIRA
     send = browser.find_element_by_xpath('/html[1]/body[1]/div[6]/div[2]/div[1]/div[2]/button[1]')
     send.click()
 
+    #Note sent to JIRA successfully
     browser.save_screenshot("Note sent to Jira.png")
     print('Note sent to JIRA successfully')
 
 except:
 
+    #Note send failed
     browser.save_screenshot("share_fail.png")
     print('Share failed')
 
